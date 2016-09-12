9572f329ad9a0bda2853ec91853d140f9e654947
blindsay <blindsay@iastate.edu>
2016-09-12 10:12:24 -0500

    Finished One way communication from client to server in go

--- go_jangle/client/client.go
+++ go_jangle/client/client.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-       "fmt"
-       "log"
-       "net"
+	"fmt"
+	"log"
+	"net"
+	"bufio"
+	"os"
 )
 
 func main(){
-     conn, err := net.Dial("tcp", "localhost:9090")
-     if err != nil {
-	log.Fatal(err)
-	}    
-     fmt.Fprintf(conn, "Hello from Client")
+	conn, err := net.Dial("tcp", "localhost:9090")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+	fmt.Fprintf(conn, "%s", text)
+	}
 }
--- go_jangle/server/server.go
+++ go_jangle/server/server.go
@@ -1,38 +1,42 @@
 package main
 
 import (
-       "fmt"
-       "net"
-       "log"
+	"fmt"
+	"log"
+	"net"
 )
 
-func main(){
-     address := "localhost:9090"
-     read_data := make([]byte, 1024)
-     
-     fmt.Println("JANGLE GO SERVER")
-     fmt.Println("address - " + address)
+func main() {
+	address := "localhost:9090"
+	read_data := make([]byte, 1024)
 
-     listener, err := net.Listen("tcp", address)
-     if err != nil{
-     	log.Fatal(err)
-     }
-     defer listener.Close()
-     for {
-     	 conn, err := listener.Accept()
-	 if err != nil {
-	    log.Fatal(err)	
-	 }
-	 go func(c net.Conn){
-	     	read_len, err := c.Read(read_data)
+	fmt.Println("JANGLE GO SERVER")
+	fmt.Println("address - " + address)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		fmt.Println("User Connected")
+		defer conn.Close()
 		if err != nil {
-		   log.Fatal(err)
+			log.Fatal(err)
 		}
-		read_string := string(read_data)
-		fmt.Printf("Read %d bytes\n", read_len);
-		fmt.Println(read_string)
-	 	c.Close()
-	 }(conn)
+		go func(c net.Conn) {
+			for {
+				read_len, err := c.Read(read_data)
+				if err != nil {
+					fmt.Println("User Disconnected")
+					break
+				}
+				read_string := string(read_data[:read_len])
+				fmt.Printf("\tRead %d bytes\n", read_len)
+				fmt.Println("\t",read_string)
+			}
+		}(conn)
 
-     }
+	}
 }
