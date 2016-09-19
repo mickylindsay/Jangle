878268fdd1e60c78e94b535d0166ab86cf126284
blindsay <blindsay@iastate.edu>
2016-09-19 12:48:35 -0500

    Moved type User to seperate file

--- go_jangle/src/server.go
+++ go_jangle/src/server.go
@@ -9,32 +9,6 @@ import (
 	"container/list"
 )
 
-type User struct {
-	c *net.Conn
-	name string
-	id int
-}
-
-func (u *User) Read(read_data []byte) (int, error){
-	return (*(*u).c).Read(read_data)
-}
-
-func (u *User) Write(write_data []byte) (int, error){
-	return (*(*u).c).Write(write_data)
-}
-
-func (u *User) Printf(format string, a ...interface{}) (int, error){
-	return fmt.Fprintf((*(*u).c), format, a...)
-}
-
-func (u *User) Scanf(format string, a ...interface{}) (int, error){
-	return fmt.Fscanf(*(*u).c, format, a...)
-}
-
-func (u *User) Message(message string) {
-	u.Printf("%s: %s", u.name, message)
-}
-
 func main() {
 	//Create new list to store every client connection
 	connections := list.New()
