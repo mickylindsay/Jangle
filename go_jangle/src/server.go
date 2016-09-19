bf1f75ba0c618b3d0956cfe935b3ef5bda334dc3
blindsay <blindsay@iastate.edu>
2016-09-19 12:45:14 -0500

    Added read and write functions for type User

--- go_jangle/src/server.go
+++ go_jangle/src/server.go
@@ -11,6 +11,28 @@ import (
 
 type User struct {
 	c *net.Conn
+	name string
+	id int
+}
+
+func (u *User) Read(read_data []byte) (int, error){
+	return (*(*u).c).Read(read_data)
+}
+
+func (u *User) Write(write_data []byte) (int, error){
+	return (*(*u).c).Write(write_data)
+}
+
+func (u *User) Printf(format string, a ...interface{}) (int, error){
+	return fmt.Fprintf((*(*u).c), format, a...)
+}
+
+func (u *User) Scanf(format string, a ...interface{}) (int, error){
+	return fmt.Fscanf(*(*u).c, format, a...)
+}
+
+func (u *User) Message(message string) {
+	u.Printf("%s: %s", u.name, message)
 }
 
 func main() {
@@ -44,12 +66,16 @@ func main() {
 		user := &User{
 			c : &conn,
 		}
+		fmt.Fprintf(*user.c, "Please enter a username:\n")
+		read_len, err := (*user.c).Read(read_data)
+		user.name = string(read_data[:read_len - 1])
+		user.id = connections.Len()
 		//Add new connection onto the end of connections list
 		elem := connections.PushBack(user)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("User Connected")
+		fmt.Println("User Connected: ", user.name)
 		//Read from client and write data to every client
 		go func(user *User, e *list.Element) {
 			for {
@@ -72,11 +98,14 @@ func main() {
 		}(user, elem)
 	}
 }
+
 //Writes a string to every connection in the list of client connections
 func write_to_clients(connections *list.List, s string){
 	//Iterate over every client
 	for e := connections.Front(); e != nil; e = e.Next() {
 		//Write data to every connection
-		fmt.Fprintf(*(e.Value.(*User).c), "%s", s)
+		e.Value.(*User).Printf("%s", s)
+
+		//fmt.Fprintf(*(e.Value.(*User).c), "%s", s)
 	}
 }
