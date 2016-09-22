6e803f91c0b4d1b3ab77382da097b4579249f981
JessWalters <jessw@iastate.edu>
2016-09-22 15:10:19 -0500

    Added basic UI With message send to server

--- java_jangle/src/com/jangle/UI/Text_UI.java
+++ java_jangle/src/com/jangle/UI/Text_UI.java
@@ -1,7 +1,53 @@
 package com.jangle.UI;
 
-public class Text_UI {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import com.jangle.communicate.Client_Communicator;
 
-	
-	//Simple text UI. Needs to get implemented for demos / testing
+
+public class Text_UI extends Application {
+
+
+    private TextArea chatArea = new TextArea();
+
+    private Parent createContent() {
+        //Setting pref height of UI on .show() call
+        chatArea.setPrefHeight(550);
+
+        //Making a network connection that connects to the server
+        Client_Communicator comms = new Client_Communicator("localhost", 9090);
+
+        TextField messageStage = new TextField();
+
+        //On event listener for submitting entered text in text box
+        messageStage.setOnAction(event -> {
+            String message = "Me: ";
+            message += messageStage.getText();
+            chatArea.appendText(message + "\n");
+            //Send the string to the server
+            comms.sendToServer(message);
+            messageStage.clear();
+        });
+
+        VBox root = new VBox(20, chatArea, messageStage);
+        root.setPrefSize(600, 600);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.show();
+    }
 }
+	//Simple text UI. Needs to get implemented for demos / testing
+
