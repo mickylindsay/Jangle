3986ec741d03ee52964d47dc49ff17c68d3383c2
Nathan Conroy <njconroy@iasate.edu>
2016-09-22 16:56:40 -0500

    Changed String inputs to byte arrays, and add throw declaration intsteasd fo try catch

--- java_jangle/src/com/jangle/communicate/Client_Communicator.java
+++ java_jangle/src/com/jangle/communicate/Client_Communicator.java
@@ -5,84 +5,57 @@ import java.net.*;
 
 public class Client_Communicator {
 
-	
 	/**
-	 * Creates a communication for the module, which can write to the write buffer, and read from the read buffer.  
+	 * Creates a communication for the module, which can write to the write
+	 * buffer, and read from the read buffer.
 	 */
-	
+
 	Socket Java_Socket;
-	
+
 	/**
 	 * Object used to write to the socket
 	 */
 	PrintWriter Write;
-	
+
 	/**
 	 * Object used to read form the socket
 	 */
 	BufferedReader Reader;
-	
-	
-	
-	public Client_Communicator(String Host, int port){
-		
-		try {
-			Java_Socket= new Socket(Host, port);
-		} catch (IOException e) {
-			System.out.println("Could not initalize Socket for communicator");
-			e.printStackTrace();
-		}
-		
-		
-		//Initialize PrintWriter to write to the output stream
-		try {
-			Write = new PrintWriter(Java_Socket.getOutputStream(), true);
-		} catch (IOException e) {
-			System.out.println("Could not initalize the writer");
-			e.printStackTrace();
-		}
-		
-		
-		//Initialize buffer reader to read from the input stream
-		try {
-			Reader = new BufferedReader(new InputStreamReader(Java_Socket.getInputStream()));
-		} catch (IOException e) {
-			System.out.println("Could not initalize the reader");
-			e.printStackTrace();
-		}
-		
-		
+
+	public Client_Communicator(String Host, int port) throws UnknownHostException, IOException {
+
+		Java_Socket = new Socket(Host, port);
+
+		// Initialize PrintWriter to write to the output stream
+
+		Write = new PrintWriter(Java_Socket.getOutputStream(), true);
+
+		// Initialize buffer reader to read from the input stream
+		Reader = new BufferedReader(new InputStreamReader(Java_Socket.getInputStream()));
 
 	}
-	
-	
-	
+
 	/**
 	 * Writes a string of data to the server
-	 * @param Message The message to send to the server
+	 * 
+	 * @param Message
+	 *            The message to send to the server
 	 */
-	public void sendToServer(String Message){
-		
+	public void sendToServer(byte[] Message) {
+
 		Write.println(Message);
-		
+
 	}
-	
+
 	/**
-	 * Reads data from the server. This is a blocking call if there is nothing to read from the server
+	 * Reads data from the server. This is a blocking call if there is nothing
+	 * to read from the server
+	 * 
 	 * @return The data read from the server
+	 * @throws IOException 
 	 */
-	public String readFromServer(){
-		try {
-			return Reader.readLine();
-		} catch (IOException e) {
-			System.out.println("Could not read from the server");
-			e.printStackTrace();
-		}
-		return "Failed to read";
+	public String readFromServer() throws IOException {
+		 return Reader.readLine();
 	}
-	
-	
-	
-	
-	
+
 }
