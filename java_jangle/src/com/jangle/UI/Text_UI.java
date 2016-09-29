ae3ff54f5728e5d175d7e3aa3876f0a1160a2c29
JessWalters <jessw@iastate.edu>
2016-09-28 20:40:16 -0500

    Cleaned up the packages and Text_UI.java file

--- java_jangle/src/com/jangle/UI/Text_UI.java
+++ java_jangle/src/com/jangle/UI/Text_UI.java
@@ -1,8 +1,6 @@
 package com.jangle.UI;
 
-import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
@@ -13,19 +11,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import com.jangle.communicate.Client_Communicator;
-import com.jangle.communicate.network_Connection;
-import com.jangle.test_Client_Server.Client;
-import com.jangle.test_Client_Server.Server;
+
 
 public class Text_UI extends Application {
-
-	private boolean isServer = true;
 	
-	private Client_Communicator comms;
+	private Client_Communicator mClientCommunicator;
 
 	private TextArea chatArea = new TextArea();
-	// private network_Connection connection = isServer ? createServer() :
-	// createClient();
 
 	private Parent createContent() {
 		// Setting pref height of UI on .show() call
@@ -34,7 +26,7 @@ public class Text_UI extends Application {
 		// Making a network connection that connects to the server
 		
 		try {
-			comms = new Client_Communicator("localhost", 9090);
+			mClientCommunicator = new Client_Communicator("localhost", 9090);
 		} catch (IOException e1) {
 			System.out.println("FAILED TO CONNECT TO SERVER");
 			e1.printStackTrace();
@@ -44,21 +36,15 @@ public class Text_UI extends Application {
 
 		// On event listener for submitting entered text in text box
 		messageStage.setOnAction(event -> {
-			String message = "";// = isServer ? "Server: " : "Client: ";
+			String message = "";
 			message += "stuff" + messageStage.getText();
 			chatArea.appendText(message + "\n");
 
-			/*
-			 * //Sending a message WALTERS try { connection.send(message); }
-			 * catch (Exception e) {
-			 * chatArea.appendText("Failed to send message! Error code: " + e +
-			 * "\n"); }
-			 */
-			// Send the string to the server CONROY
-			if (comms != null) {
+			// Send the string to the server
+			if (mClientCommunicator != null) {
 				try {
 					System.out.println(message);
-					comms.sendToServer(message.getBytes("UTF-8"), {0000}, {0000});
+					mClientCommunicator.sendToServer(message.getBytes("UTF-8"), {0000}, {0000});
 				} catch (Exception e) {
 					chatArea.appendText("Failed to send message! Error code: " + e + "\n");
 				}
@@ -75,43 +61,12 @@ public class Text_UI extends Application {
 		launch(args);
 	}
 
-	/*
-	@Override
-	public void init() throws Exception {
-		connection.startConnection();
-	}
-	*/
-
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setScene(new Scene(createContent()));
 		primaryStage.show();
 	}
 
-	/*
-	@Override
-	public void stop() throws Exception {
-		connection.closeConnection();
-	}
-	
-
-	private Server createServer() {
-		return new Server(7878, data -> {
-			Platform.runLater(() -> {
-				addMessage(data.toString() + "\n");
-			});
-		});
-	}
-
-	private Client createClient() {
-		return new Client("127.0.0.1", 7878, data -> {
-			Platform.runLater(() -> {
-				addMessage(data.toString() + "\n");
-			});
-		});
-		
-	}
-*/
 	public void addMessage(String message) {
 		chatArea.appendText(message);
 	}
