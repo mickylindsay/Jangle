c2d6c757f066f747a348503152d58aee3152488c
JessWalters <jessw@iastate.edu>
2016-09-28 20:30:18 -0500

    refined the user class

--- java_jangle/src/com/jangle/client/User.java
+++ java_jangle/src/com/jangle/client/User.java
@@ -5,4 +5,44 @@ package com.jangle.client;
  */
 public class User {
 
+    private int id;
+    private int status;
+    private String displayName;
+
+    public User(String displayName, int id, int status) {
+        this.displayName = displayName;
+        this.id = id;
+        this.status = status;
+    }
+
+    public User(String displayName, int id) {
+        this.displayName = displayName;
+        this.id = id;
+        this.status = 0;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
 }
