4c5d0028153ffb30b2fa49a1aba4e46bd2b75b68
bindsay <blindsay@iastate.edu>
2016-10-04 13:42:39 -0500

    Finished Message inserting

--- go_jangle/src/Database.go
+++ go_jangle/src/Database.go
@@ -11,19 +11,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-/*func main() {
-	//Attempt to connect to MySQL Database
-	db, err := Connect_Database()
-	if err != nil {
-		log.Fatal(err)
-	}
-	//Test MySQL Execution
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users(userid INT,	username VARCHAR(20),	displayname VARCHAR(20),	imagepath VARCHAR(50),passwordhash VARCHAR(50),	salt VARCHAR(8),	status CHAR(1),	PRIMARY KEY (userid));")
-	if err != nil {
-		log.Fatal(err)
-	}
-}*/
-
 //Returns a connection to the mysql database at the location either prompted or found in the file .databasedsn in the directory of the executable
 func Connect_Database() (*sql.DB, error){
 	//Finds executables current directory and read the data in .databasedsn
@@ -41,30 +28,47 @@ func Connect_Database() (*sql.DB, error){
 	}
 	//Attempts to open a conncetion to the mysql database
 	db , err := sql.Open("mysql", dsn);
-	//_, err = jangle.db.Exec("DROP TABLE messages");
-	//_, err = jangle.db.Exec("CREATE TABLE messages (userid INT UNSIGNED, time INT UNSIGNED, messageid INT UNSIGNED, messagetext VARCHAR(2147483647), serverid INT UNISIGNED, roomid INT UNSIGNED  PRIMARY KEY (messageid),	FOREIGN KEY (userid) REFERENCES users (userid), FOREIGN KEY (serverid) REFERENCES servers (serverid), FOREIGN KEY (roomid) REFERENCES rooms (roomid));");
-
 	return db, err
 }
 
+//Returns userid from database when correct username and password(hash) is given
 func User_Login(u []byte, p []byte) (uint, error) {
 	var userid uint;
 	err := jangle.db.QueryRow("SELECT userid FROM users WHERE username =? AND passwordhash=?",string(u), string(p)).Scan(&userid);
 	return userid, err;
 }
 
+//Returnst the next valid userid
 func Next_Userid() uint{
 	var temp uint;
 	_ = jangle.db.QueryRow("SELECT MAX(userid) AS userid FROM users").Scan(&temp);
 	return temp;
 }
 
+//Returns the next valid messageid
+func Next_Messageid() uint{
+	var temp uint;
+	_ = jangle.db.QueryRow("SELECT MAX(messageid) AS messageid FROM messages").Scan(&temp);
+	return temp;
+}
+
+//Inserts a new user into the database
+//TODO implement Image Path and Password hashing
 func User_Create(u []byte, p []byte) error{
 	_, err := jangle.db.Exec("INSERT INTO users (userid, username, displayname, imagepath, passwordhash, salt) VALUES (?,?,?,?,?,?);",Next_Userid()+1, string(u), string(u), "TEMPPATH", string(p), "0000");
 	return err;
 }
 
-/*func Write_Message() error{
-	_, err := jangle.db.Exec("INSERT INTO messages ()")
-}*/
+//Inserts a new Message into the database
+//TODO implement roomid and serverid
+func Message_Create(userid uint, messagetext []byte) error{
+	_, err := jangle.db.Exec("INSERT INTO messages (userid, time, messageid, messagetext, serverid, roomid) VALUES (?,?,?,?,?,?);", userid, Milli_Time(), Next_Messageid() + 1, string(messagetext), 0, 0);
+	return err;
+}
 
+//TODO
+//Request chunks of 50 messages offset by (offset*50) and returns them as array of message objects
+func Request_n_Messages(offset uint) []Message{
+	messages := make([]Message,50);
+	return messages;
+}
