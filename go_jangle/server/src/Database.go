1e3aebee0e7abbed42d7a4d2ba4dc631fd69475b
bindsay <blindsay@iastate.edu>
2016-10-07 19:35:34 -0500

    Added no_database flag

--- go_jangle/server/src/Database.go
+++ go_jangle/server/src/Database.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	//"log"	
 	"os"
 	"bufio"
 	"io/ioutil"
@@ -13,102 +12,136 @@ import (
 
 //Returns a connection to the mysql database at the location either prompted or found in the file .databasedsn in the directory of the executable
 func Connect_Database() (*sql.DB, error){
-	//Finds executables current directory and read the data in .databasedsn
-	var dsn string
-	dir, _ := filepath.Abs(filepath.Dir(os.Args[0])) 
-	dat, err := ioutil.ReadFile(dir + "/../.databasedsn")
-	//If such file does not exist prompt the user to enter a DSN
-	if err != nil{
-		fmt.Println("Please enter mysql database DSN:")
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		dsn = text[:len(text)-1]
-	}else{
-		dsn = string(dat)
+	if(!jangle.no_database){
+		fmt.Println("Connecting to MySQL Database.")
+		//Finds executables current directory and read the data in .databasedsn
+		var dsn string;
+		dir, _ := filepath.Abs(filepath.Dir(os.Args[0]));
+		dat, err := ioutil.ReadFile(dir + "/../.databasedsn");
+		//If such file does not exist prompt the user to enter a DSN
+		if err != nil{
+			fmt.Println("Please enter mysql database DSN:");
+			reader := bufio.NewReader(os.Stdin);
+			text, _ := reader.ReadString('\n');
+			dsn = text[:len(text)-1];
+		}else{
+			dsn = string(dat);
+		}
+		//Attempts to open a conncetion to the mysql database
+		db , err := sql.Open("mysql", dsn);
+		if(err == nil){
+			fmt.Println("Database Connection Successful.");
+		}else{
+			fmt.Println("Failed to connect to Database.");
+			fmt.Println("Running in 'no_database' mode.");
+			jangle.no_database = true;
+		}
+		return db, err;
 	}
-	//Attempts to open a conncetion to the mysql database
-	db , err := sql.Open("mysql", dsn);
-	return db, err
+	fmt.Println("Running in 'no_database' mode.")
+	return nil, nil;
+	
 }
 
 //Returns userid from database when correct username and password(hash) is given
 func User_Login(u []byte, p []byte) (uint, error) {
-	var userid uint;
-	err := jangle.db.QueryRow("SELECT userid FROM users WHERE username =? AND passwordhash=?",string(u), string(p)).Scan(&userid);
-	return userid, err;
+	if(!jangle.no_database){
+		var userid uint;
+		err := jangle.db.QueryRow("SELECT userid FROM users WHERE username =? AND passwordhash=?",string(u), string(p)).Scan(&userid);
+		return userid, err;
+	}
+	return 1, nil;
 }
 
 //Returnst the next valid userid
 func Request_Display_Name(id uint) []byte{
-	var temp []byte;
-	_ = jangle.db.QueryRow("SELECT displayname FROM users WHERE userid = ?", id).Scan(&temp);
-	return temp;
+	if(!jangle.no_database){
+		var temp []byte;
+		_ = jangle.db.QueryRow("SELECT displayname FROM users WHERE userid = ?", id).Scan(&temp);
+		return temp;
+	}
+	return []byte("Display Name")
 }
 
 //Returnst the next valid userid
 func Next_Userid() uint{
-	var temp uint;
-	_ = jangle.db.QueryRow("SELECT MAX(userid) AS userid FROM users").Scan(&temp);
-	return temp + 1;
+	if(!jangle.no_database){
+		var temp uint;
+		_ = jangle.db.QueryRow("SELECT MAX(userid) AS userid FROM users").Scan(&temp);
+		return temp + 1;
+	}
+	return 1;
 }
 
 //Returns the next valid messageid
 func Next_Messageid() uint{
-	var temp uint;
-	_ = jangle.db.QueryRow("SELECT MAX(messageid) AS messageid FROM messages").Scan(&temp);
-	return temp + 1;
+	if(!jangle.no_database){
+		var temp uint;
+		_ = jangle.db.QueryRow("SELECT MAX(messageid) AS messageid FROM messages").Scan(&temp);
+		return temp + 1;
+	}
+	return 1;
 }
 
 //Inserts a new user into the database
 //TODO implement Image Path and Password hashing
 func User_Create(u []byte, p []byte) error{
-	_, err := jangle.db.Exec("INSERT INTO users (userid, username, displayname, imagepath, passwordhash, salt) VALUES (?,?,?,?,?,?);",Next_Userid(), string(u), string(u), "TEMPPATH", string(p), "0000");
-	return err;
+	if(!jangle.no_database){
+		_, err := jangle.db.Exec("INSERT INTO users (userid, username, displayname, imagepath, passwordhash, salt) VALUES (?,?,?,?,?,?);",Next_Userid(), string(u), string(u), "TEMPPATH", string(p), "0000");
+		return err;
+	}
+	return nil;
 }
 
 //Inserts a new Message into the database
 //TODO implement roomid and serverid
 func Message_Create(user *User, messagetext []byte) error{
-	var err error
-	if(user.id == 0){
-		_, err = jangle.db.Exec("INSERT INTO messages (userid, time, messageid, messagetext, serverid, roomid) VALUES (?,?,?,?,?,?);", 1, Milli_Time(), Next_Messageid(), string(messagetext), 1, 1);
-	}else{
-	_, err = jangle.db.Exec("INSERT INTO messages (userid, time, messageid, messagetext, serverid, roomid) VALUES (?,?,?,?,?,?);", user.id, Milli_Time(), Next_Messageid(), string(messagetext), 1, 1);
+	if(!jangle.no_database){
+		var err error
+		if(user.id == 0){
+			_, err = jangle.db.Exec("INSERT INTO messages (userid, time, messageid, messagetext, serverid, roomid) VALUES (?,?,?,?,?,?);", 1, Milli_Time(), Next_Messageid(), string(messagetext), 1, 1);
+		}else{
+			_, err = jangle.db.Exec("INSERT INTO messages (userid, time, messageid, messagetext, serverid, roomid) VALUES (?,?,?,?,?,?);", user.id, Milli_Time(), Next_Messageid(), string(messagetext), 1, 1);
+		}
+		return err;
 	}
-	return err;
+	return nil;
 }
 
 //Request chunks of 50 messages offset by (offset*50) and returns them as array of message objects
 func Request_Offset_Messages(offset uint) ([]Message, error){
-	i := 0;
-	messages := make([]Message,50);
-	var(
-		time_read uint
-		text_read string
-		userid_read uint
-	)
-	//Query 50 rows of messages
-	rows, err := jangle.db.Query("SELECT userid, time, messagetext FROM messages ORDER BY messageid DESC LIMIT 50 OFFSET  ?", offset*50)
-	Check_Error(err);
-	defer rows.Close();
-	//Iterate through the rows
-	for rows.Next() {
-		//Scan the columns into variables
-		err := rows.Scan(&userid_read, &time_read, &text_read);
-		fmt.Println(text_read);
+	if(!jangle.no_database){
+		i := 0;
+		messages := make([]Message,50);
+		var(
+			time_read uint
+			text_read string
+			userid_read uint
+		)
+		//Query 50 rows of messages
+		rows, err := jangle.db.Query("SELECT userid, time, messagetext FROM messages ORDER BY messageid DESC LIMIT 50 OFFSET  ?", offset*50)
 		Check_Error(err);
-		//Create a "17" message to send back to user
-		m := Message_Recieve{
-			code: 17,
-			serverid: Int_Converter(0),
-			roomid: Int_Converter(0),
-			userid: Int_Converter(userid_read),
-			time: Int_Converter(time_read),
-			text: []byte(text_read)};
-		//Add that message to the array which will be returned
-		messages[i] = m;
-		i++;
+		defer rows.Close();
+		//Iterate through the rows
+		for rows.Next() {
+			//Scan the columns into variables
+			err := rows.Scan(&userid_read, &time_read, &text_read);
+			fmt.Println(text_read);
+			Check_Error(err);
+			//Create a "17" message to send back to user
+			m := Message_Recieve{
+				code: 17,
+				serverid: Int_Converter(0),
+				roomid: Int_Converter(0),
+				userid: Int_Converter(userid_read),
+				time: Int_Converter(time_read),
+				text: []byte(text_read)};
+			//Add that message to the array which will be returned
+			messages[i] = m;
+			i++;
+		}
+		//Return array of messages
+		return messages, err;
 	}
-	//Return array of messages
-	return messages, err;
+	return nil, nil;
 }
--- go_jangle/server/src/Server.go
+++ go_jangle/server/src/Server.go
@@ -12,14 +12,15 @@ type Jangle struct {
 	userlist *list.List
 	db *sql.DB
 	address string
-	debug boolean
+	debug bool
+	no_database bool
 }
 
 var jangle Jangle;
 
 func main() {
-	Init_Server();
 	Init_Flags();
+	Init_Server();
 	
 	fmt.Println("\x1b[0;31mJANGLE GO SERVER");
 	fmt.Println("\x1b[0;0mlistening on - " + jangle.address);
@@ -50,13 +51,10 @@ func main() {
 func Init_Server(){
 	//Create new list to store every client connection
 	jangle.userlist = list.New();
-
 	//Make connection to Database
-	fmt.Println("Connecting to MySQL Database.")
 	var e error;
 	jangle.db, e = Connect_Database();
 	Check_Error(e);
-	fmt.Println("Database Connection Successful.")
 
 }
 
@@ -69,8 +67,12 @@ func Init_Flags(){
 
 	debug_flag := flag.Bool("debug", false, "Puts server in debug mode");
 
+	no_database_flag := flag.Bool("nodb", false, "Turns off connection to database");
+
+	
 	flag.Parse();
 
 	jangle.address = *address_flag + ":" + *port_flag;
 	jangle.debug = *debug_flag;
+	jangle.no_database = *no_database_flag;
 }
