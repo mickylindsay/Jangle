bbab350191dc202e3a987f628adb0bc6b1d23e62
bindsay <blindsay@iastate.edu>
2016-10-13 17:04:06 -0500

    Updated build.bat

--- go_jangle/server/build.bat
+++ go_jangle/server/build.bat
@@ -1,8 +1,5 @@
 @echo off
 IF NOT EXIST bin
    mkdir bin
-set obj = User.go Message.go Database.go Util.go Parse.go Communication.go
-IF "%1"=="-c" 
-   go build -o bin/server.exe Server.go %obj%
-ELSE
-   go build -o bin/client.exe Client.go %obj%
+go build -o bin/server.exe Server.go User.go Message.go Database.go Util.go Parse.go Communication.go
+
