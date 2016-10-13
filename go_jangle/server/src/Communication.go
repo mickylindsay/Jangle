b99b33cc77ca1351826f6086dc63f9b7be25d15c
bindsay <blindsay@iastate.edu>
2016-10-13 13:55:05 -0500

    Moved user functions to User.go

--- go_jangle/server/src/Communication.go
+++ go_jangle/server/src/Communication.go
@@ -76,19 +76,3 @@ func Send_Broadcast_Server_Room (serverid uint, roomid uint, message Message) {
 		}
 	}			
 }
-
-func (u *User) Read (read_data []byte) (int, error) {
-	return (*(*u).c).Read(read_data)
-}
-
-func (u *User) Write (write_data []byte) (int, error) {
-	return (*(*u).c).Write(write_data)
-}
-
-func (u *User) Printf (format string, a ...interface{}) (int, error) {
-	return fmt.Fprintf((*(*u).c), format, a...)
-}
-
-func (u *User) Scanf (format string, a ...interface{}) (int, error) {
-	return fmt.Fscanf(*(*u).c, format, a...)
-}
