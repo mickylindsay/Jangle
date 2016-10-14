736f25324e877bea933435076c2cb37d422edd3b
bindsay <blindsay@iastate.edu>
2016-10-14 16:43:48 -0500

    Began implementing password hashing

---
+++ go_jangle/server/src/Crypt.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Password_Crypt(password []byte) ([]byte, error){
+	return bcrypt.GenerateFromPassword(password, 10);
+}
+
+func Compair_Crypt(password []byte, hash []byte) error{
+	return bcrypt.CompareHashAndPassword(hash, password);
+}
