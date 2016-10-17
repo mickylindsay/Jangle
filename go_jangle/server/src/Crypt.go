985e872d2e2f286f2e8153836a419609597abac6
bindsay <blindsay@iastate.edu>
2016-10-17 10:45:03 -0500

    Added salt to Compair_Crypt function

--- go_jangle/server/src/Crypt.go
+++ go_jangle/server/src/Crypt.go
@@ -8,6 +8,9 @@ func Password_Crypt(password []byte) ([]byte, error){
 	return bcrypt.GenerateFromPassword(password, 10);
 }
 
-func Compair_Crypt(password []byte, hash []byte) error{
-	return bcrypt.CompareHashAndPassword(hash, password);
+func Compair_Crypt(password []byte, salt []byte, hash []byte) error{
+	salted_password := make([]byte, len(password) + len(salt));
+	copy(salted_password[:len(password)], password[:]);
+	copy(salted_password[len(password):], salt[:]);	
+	return bcrypt.CompareHashAndPassword(hash, salted_password);
 }
