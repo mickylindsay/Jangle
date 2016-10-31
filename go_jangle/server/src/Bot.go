de038235d20a999979f72afffab26bfc88c7f29d
bindsay <blindsay@iastate.edu>
2016-10-31 11:50:36 -0500

    Implemented Bot_Move_User

--- go_jangle/server/src/Bot.go
+++ go_jangle/server/src/Bot.go
@@ -1,10 +1,31 @@
 package main
 
 import (
-
+	"fmt"
 )
 
 type Bot struct {
 	serverid uint8
 }
 
+func (b *Bot) Bot_Move_User(userid uint, roomid uint) error {
+	for e := jangle.userlist.Front(); e != nil; e = e.Next() {
+		if (e.Value.(*User).id == userid) {
+			e.Value.(*User).roomid = roomid;
+			return nil;
+		}
+	}
+	return fmt.Errorf("Bot %d: Unable to Move User %d. No such User", b.serverid, userid);
+}
+
+//Possibly create message 18 Bot message
+/*
+func (b *Bot) Bot_Broadcast(text []byte, roomid uint){
+	m := Message_Recieve{
+		code: 17,
+		serverid: Int_Converter(b.serverid),
+		roomid: Int_Converter(roomid),
+		userid: Int_Converter()
+	}
+}
+*/
