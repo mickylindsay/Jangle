c48b21de021bef66f53e57e7dbf53182a151897b
Tom Frye <Tom Frye>
2016-11-07 15:40:13 -0600

    Fixed comments

--- go_jangle/server/src/Bot.go
+++ go_jangle/server/src/Bot.go
@@ -4,33 +4,44 @@ import (
 	"strings"
 )
 
-//TODO
+//Initializes function array that contains all the functions necessary to handle all
+//the bot commands
 func Init_Command () {
-	Commands := make([]func(args []string), 4)
+	Commands := make([]func(args []string), 5)
 
 	Commands[0] = Kick_User
 	Commands[1] = Mute_User
 	Commands[2] = Unmute_User
 	Commands[3] = Move_User
+	Commands[4] = Prune_N_Messages
 
 	jangle.Commands = Commands
 }
 
-//TODO
+//Checks if the text part of the byte array from a message code type 16, message 
+//client send, is a bot command
+//If the text is bot command, the appropiate function is called from the function array
+//that will execute the bot command properly
+//If the text is not a bot command, the message code type 16 is handled normally
 func Check_Command (user *User, data []byte) bool {
 	var check bool
 	if (string(data[0]) == "/") {
-		check = true
 		args := strings.Split(string(data), " ")
 		trigger := Switcher(args[0])
-		jangle.Commands[trigger](args[1:])
+		if (trigger != 255) {
+			check = true
+			jangle.Commands[trigger](args[1:])
+		} else {
+			check = false
+		}	
 	} else {
 		check = false
 	}
 	return check
 }
 
-//TODO
+//Changes the trigger word from the bot command to its corresponding byte value to
+//reference the funciton array
 func Switcher (s string) byte {
 	var switcher byte
 	switch {
@@ -42,11 +53,15 @@ func Switcher (s string) byte {
 			switcher = 2
 		case s == "move":
 			switcher = 3
+		case s == "prune":
+			switcher = 4
+		default:
+			switcher = 255
 	}
 	return switcher
 }
 
-//TODO
+//Kicks the user from the server
 func Kick_User (args []string) {
 	c := Kick{
 		user: Get_User_From_Userid(Byte_Converter([]byte(args[1])))}
@@ -54,7 +69,7 @@ func Kick_User (args []string) {
 	c.Send()
 }
 
-//TODO
+//Mutes the user
 func Mute_User (args []string) {
 	c := Mute{
 		user: Get_User_From_Userid(Byte_Converter([]byte(args[1])))}
@@ -62,7 +77,7 @@ func Mute_User (args []string) {
 	c.Send()
 }
 
-//TODO
+//Unmutes the user
 func Unmute_User (args []string) {
 	c := Unmute{
 		user: Get_User_From_Userid(Byte_Converter([]byte(args[1])))}
@@ -70,7 +85,7 @@ func Unmute_User (args []string) {
 	c.Send()
 }
 
-//TODO
+//Moves the user to a different room
 func Move_User (args []string) {
 	c := Move{
 		user: Get_User_From_Userid(Byte_Converter([]byte(args[1]))),
@@ -80,24 +95,30 @@ func Move_User (args []string) {
 }
 
 //TODO
+func Prune_N_Messages (args []string) {
+
+}
+
+//Constructs generic Command type
 type Command interface {
 	Execute()
 	Send()
 }
 
-//TODO
+//Creates Kick struct with param User type
 type Kick struct {
 	user *User
 }
 
-//TODO
+//Sets the user's severid and roomid to the default value and removes the user from
+//the user list
 func (c Kick) Execute() {
 	c.user.serverid = uint(default_value)
 	c.user.roomid = uint(default_value)
 	Remove_User_From_Userlist(c.user.id);
 }
 
-//TODO
+//Builds a message code type 97, broadcast server
 func (c Kick) Send() {
 	m := Serverid_Userid{
 		code: broadcast_server,
@@ -106,17 +127,17 @@ func (c Kick) Send() {
 	Message97(c.user, m.Build_Message())
 }
 
-//TODO
+//Creates Mute struct with param User type
 type Mute struct {
 	user *User
 }
 
-//TODO
+//Changes the user's muted to status to muted
 func (c Mute) Execute() {
 	c.user.muted = user_muted
 }
 
-//TODO
+//Builds a message code type 96, broadcast status
 func (c Mute) Send() {
 	m := Userid_Status{
 		code: broadcast_status,
@@ -126,17 +147,17 @@ func (c Mute) Send() {
 	Message96(c.user, m.Build_Message())
 }
 
-//TODO
+//Creates Unmute struct with param User type
 type Unmute struct {
 	user *User
 }
 
-//TODO
+//Changes the user's muted status to unmuted
 func (c Unmute) Execute() {
 	c.user.muted = user_unmuted
 }
 
-//TODO
+//Builds a message code type 96, broadcast status
 func (c Unmute) Send() {
 	m := Userid_Status{
 		code: broadcast_status,
@@ -146,22 +167,37 @@ func (c Unmute) Send() {
 	Message96(c.user, m.Build_Message())
 }
 
-//TODO
+//Creates Move struct with param User type and roomid as an uint
 type Move struct {
 	user *User
 	roomid uint
 }
 
-//TODO
+//Sets the user's roomid to the new roomid
 func (c Move) Execute() {
 	c.user.roomid = c.roomid
 }
 
-//TODO
+//Builds a message code type 98, broadcast room
 func (c Move) Send() {
 	m := Roomid_Userid{
 		code: broadcast_room,
 		roomid: Int_Converter(c.user.roomid),
 		userid: Int_Converter(c.user.id)}
 	Message98(c.user, m.Build_Message())
+}
+
+//TODO
+type Prune struct {
+	user *User
+}
+
+//TODO
+func (c Prune) Execute() {
+
+}
+
+//TODO
+func (c Prune) Send() {
+
 }
