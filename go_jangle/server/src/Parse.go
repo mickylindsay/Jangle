4d357ebd1f43c48be82f5a587e8ab254fb670da1
Thomas <tjfrye@iastate.edu>
2016-11-27 02:44:35 -0600

    stuff

--- go_jangle/server/src/Parse.go
+++ go_jangle/server/src/Parse.go
@@ -76,7 +76,7 @@ func Login_Message(user *User, data []byte) Message {
 
 //TODO
 func Standard_Message(user *User, data []byte) Message {
-	m := Create_Message(message_client_send, data[1:5], data[6:10], data[11:15], data[16:])
+	m := Create_Message(message_client_send, data[1:5], data[5:9], data[9:13], data[13:])
 	if user.muted != 1 {
 		messageid, err := Message_Create(user, m.text)
 		if err != nil {
